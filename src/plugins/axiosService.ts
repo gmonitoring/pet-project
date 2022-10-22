2e5655d3b99f0a59400760200c6b23328bffe31e
NikitaMarchuk <gmonitoring.github@gmail.com>
2022-10-23 00:05:30 +0500

    fix: del unused interceptors, fix getFavoriteHouses

--- src/plugins/axiosService.ts
+++ src/plugins/axiosService.ts
@@ -10,33 +10,10 @@ const httpClient = axios.create({
 });
 
 httpClient.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-   */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
-
   response => {
     return response.data;
   },
   error => {
-    return Promise.reject(error);
-  },
-);
-
-httpClient.interceptors.request.use(
-  config => {
-    // config.headers['Authorization'] = 'Bearer '
-
-    return config;
-  },
-  error => {
-    console.log(error); // for debugging
     return Promise.reject(error);
   },
 );
