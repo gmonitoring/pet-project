7d18e2d305322900196f7e90fd394c4c61f2f768
NikitaMarchuk <gmonitoring.github@gmail.com>
2022-11-18 13:28:11 +0500

    rename-project

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useRootStore } from '../../hooks/useRootStore';
 import { observer } from 'mobx-react';
 import Box from '@mui/material/Box';
